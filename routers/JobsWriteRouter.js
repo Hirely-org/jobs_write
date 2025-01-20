b3f6e275ee27fcb256a5ea86d9e554a72488b3a5
Škrbić Veljko <494716@student.fontys.nl>
2025-01-20 14:12:23 +0100

    awdaw

--- routers/JobsWriteRouter.js
+++ routers/JobsWriteRouter.js
@@ -23,33 +23,33 @@ router.get('/test', async (req, res) => {
     return res.send('Hello World!');
 });
 
-router.post('/', async (req, res) => {
-    const { name, description, status, imageId } = req.body;
-    const userRole = req.headers['x-forwarded-role'];
-    console.log('Headers:', req.headers);
-    console.log('User role:', userRole);
+// router.post('/', async (req, res) => {
+//     const { name, description, status, imageId } = req.body;
+//     const userRole = req.headers['x-forwarded-role'];
+//     console.log('Headers:', req.headers);
+//     console.log('User role:', userRole);
 
-    if(!["Admin", "Employer"].includes(userRole)){
-        return res.status(403).send('Forbidden');
-    }
+//     if(!["Admin", "Employer"].includes(userRole)){
+//         return res.status(403).send('Forbidden');
+//     }
 
-    let job;
-    try{
-        job = await db.Job.create({
-            name,
-            description,
-            status,
-            imageId,
-            createdAt: new Date()
-        });
-    } catch(error){
-        console.error('Error creating job:', error);
-        return res.status(500).send('Error creating job');
-    }
-    console.log('Created job:', job.name);
-    RabbitMQService.sendToQueue("q_a", Buffer.from(JSON.stringify(job)));
-    return res.json(job);
-});
+//     let job;
+//     try{
+//         job = await db.Job.create({
+//             name,
+//             description,
+//             status,
+//             imageId,
+//             createdAt: new Date()
+//         });
+//     } catch(error){
+//         console.error('Error creating job:', error);
+//         return res.status(500).send('Error creating job');
+//     }
+//     console.log('Created job:', job.name);
+//     RabbitMQService.sendToQueue("q_a", Buffer.from(JSON.stringify(job)));
+//     return res.json(job);
+// });
 
 
 router.post('/', upload.single('image'), async (req, res) => {
