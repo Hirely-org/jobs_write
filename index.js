0651d83ae82dd334af5276451bd684e568332a18
Škrbić Veljko <494716@student.fontys.nl>
2025-01-21 15:38:37 +0100

    asf

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = 5000;
 
 const jobWriteRouter = require('./routers/JobsWriteRouter');
 
-app.use();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/jobWrite", jobWriteRouter);
